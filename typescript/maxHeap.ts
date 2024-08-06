859707a622f2a84e65d7753f8c3ec74d5741ebf5
Brian Hernandez <ricardo.hernandez@rightbalance.io>
2024-08-06 03:28:43 -0600

    examples completed

--- typescript/maxHeap.ts
+++ typescript/maxHeap.ts
@@ -22,6 +22,10 @@ const flat = (a: (number | any[])[]): number[] => {
 class MaxHeap {
 	private h: number[] = [];
 
+	size(): number {
+		return this.h.length;
+	}
+
 	swap(x: number, y: number): void {
 		[this.h[x], this.h[y]] = [this.h[y], this.h[x]];
 	}
@@ -50,18 +54,25 @@ class MaxHeap {
 	}
 
 	sinkDown(i: number): void {
-		// pending implementation
+		let lai = i;
+		const [l, r] = [1, 2].map(v => 2 * i + v);
+		if (l < this.h.length && this.h[l] > this.h[lai]) lai = l;
+		if (r < this.h.length && this.h[r] > this.h[lai]) lai = r;
+		if (lai !== i) {
+			this.swap(i, lai);
+			this.sinkDown(lai);
+		}
 	}
 }
 
 // complex strucure with nested arrays
 const s = [
-	-5, -20,
+	-5, -21,
 	[
-		7, -7, 2,
+		7, -71, 2,
 	],
 	[
-		[-1, 0, -10, 20],
+		[-1, 0, -10, 25],
 		[8],
 		[],
 		[11, 3]
@@ -79,3 +90,36 @@ const s = [
 
 const flatten = flat(s);
 console.log("flaten array", flatten);
+
+// find kth smallest element using fixed size max heap approach
+let k = 2;
+const h = new MaxHeap();
+
+for (const v of flatten) {
+	h.push(v);
+	if (h.size() > k) h.pop();
+}
+console.log(`${k}th smallest element is`, h.pop());
+
+// find kth largest element using k calls to pop (max heap)
+k = 3;
+const h2 = new MaxHeap();
+for (const v of flatten) h2.push(v);
+for (let i = 1; i < k; i++) h2.pop();
+console.log(`${k}th largest element is`, h2.pop());
+
+// find kth largest element using fixed size min heap approach
+k = 1;
+const h3 = new MaxHeap();
+for (const v of flatten) {
+	h3.push(-v);
+	if (h3.size() > k) h3.pop();
+}
+console.log(`${k}th largest element is`, -h3.pop()!);
+
+// find kth smallest element using k calls to pop (min heap)
+k = 4;
+const h4 = new MaxHeap();
+for (const v of flatten) h4.push(-v);
+for (let i = 1; i < k; i++) h4.pop();
+console.log(`${k}th smallest element is`, -h4.pop()!);
