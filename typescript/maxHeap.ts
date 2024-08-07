1133665d31c923916a07f90fb256cba77df1f1f1
Marco Dali <70110654+marcodali@users.noreply.github.com>
2024-08-06 22:19:19 -0600

    Update maxHeap.ts

--- typescript/maxHeap.ts
+++ typescript/maxHeap.ts
@@ -1,125 +1,76 @@
-/**
- * This recursive function will flatten any nested array of numbers
- */
-const flat = (a: (number | any[])[]): number[] => {
-	const res: number[] = [];
-	for (const item of a) {
-		if (Number.isInteger(item)) {
-			res.push(item as number);
-		} else if (Array.isArray(item)) {
-			res.push(...flat(item));
-		} else {
-			throw new Error(`cannot parse "${typeof item}" only number and array allowed`);
-		}
-	}
-	return res;
-}
-
-/**
- * This is a max heap implementation in TypeScript
- * because the greatest element is always the root (index 0)
- */
 class MaxHeap {
-	private h: number[] = [];
-
-	size(): number {
-		return this.h.length;
-	}
-
-	swap(x: number, y: number): void {
-		[this.h[x], this.h[y]] = [this.h[y], this.h[x]];
-	}
-
-	push(v: number): void {
-		if (!Number.isSafeInteger(v)) throw new Error("only safe integers allowed");
-		
-		this.h.push(v);
-		let i = this.h.length - 1;
-		while (i > 0) {
-			const p = Math.floor((i - 1) / 2);
-			if (this.h[i] <= this.h[p]) break;
-			this.swap(i, p);
-			i = p;
-		}
-	}
-
-	pop(): number | undefined {
-		if (this.h.length > 1) {
-			const max = this.h[0];
-			this.h[0] = this.h.pop() as number;
-			this.sinkDown(0);
-			return max;
-		}
-		return this.h.pop();
-	}
-
-	sinkDown(i: number): void {
-		let lai = i;
-		const [l, r] = [1, 2].map(v => 2 * i + v);
-		if (l < this.h.length && this.h[l] > this.h[lai]) lai = l;
-		if (r < this.h.length && this.h[r] > this.h[lai]) lai = r;
-		if (lai !== i) {
-			this.swap(i, lai);
-			this.sinkDown(lai);
-		}
-	}
+    private h: number[] = [];
+    private swap(x: number, y: number): void {
+        [this.h[x], this.h[y]] = [this.h[y], this.h[x]];
+    }
+    size(): number {
+        return this.h.length;
+    }
+    push(v: number): void {
+        if (!Number.isSafeInteger(v)) throw new Error("only safe integers allowed");
+        this.h.push(v);
+        this.bubbleUp(this.h.length - 1);
+    }
+    private bubbleUp(i: number) {
+        while (i > 0) {
+            const p = Math.floor((i-1)/2);
+            if (this.h[i] <= this.h[p]) break;
+            this.swap(i, p);
+            i = p;
+        }
+    }
+    pop(): number | undefined {
+        if (this.h.length > 1) {
+            const max = this.h[0];
+            this.h[0] = this.h.pop()!;
+            this.sinkDown(0);
+            return max;
+        }
+        return this.h.pop();
+    }
+    private sinkDown(i: number): void {
+        let max = i;
+        const [l, r] = [1, 2].map(v => i * 2 + v);
+        if (l < this.size() && this.h[l] > this.h[max]) max = l;
+        if (r < this.size() && this.h[r] > this.h[max]) max = r;
+        if (max !== i) {
+            this.swap(max, i);
+            this.sinkDown(max);
+        }
+    }
 }
 
-// complex strucure with nested arrays
-const s = [
-	-5, -21,
-	[
-		7, -71, 2,
-	],
-	[
-		[-1, 0, -10, 25],
-		[8],
-		[],
-		[11, 3]
-	],
-	[
-		[
-			[
-				[
-					-4, 6,
-				]
-			]
-		]
-	]
-];
+const arr = [7, -1, 3, -11, 8, 28, 38, -40, 0, 2, -5];
+let k = 1;
 
-const flatten = flat(s);
-console.log("flaten array", flatten);
-
-// find kth smallest element using fixed size max heap approach
-let k = 2;
-const h = new MaxHeap();
-
-for (const v of flatten) {
-	h.push(v);
-	if (h.size() > k) h.pop();
+/**
+ * Problem: Calculate the kth smallest item in the array
+ */
+// solution [1] fixed window size
+const h1 = new MaxHeap();
+for (const item of arr) {
+    h1.push(item);
+    if (h1.size() > k) h1.pop();
 }
-console.log(`${k}th smallest element is`, h.pop());
-
-// find kth largest element using k calls to pop (max heap)
-k = 3;
+console.log(`${k}th smallest item is ${h1.pop()}`);
+// solution [2] k calls to pop
 const h2 = new MaxHeap();
-for (const v of flatten) h2.push(v);
-for (let i = 1; i < k; i++) h2.pop();
-console.log(`${k}th largest element is`, h2.pop());
+for (const item of arr) h2.push(-item);
+for (let i = 1; i < k; i += 1) h2.pop();
+console.log(`${k}th smallest item is ${-h2.pop()!}`)
 
-// find kth largest element using fixed size min heap approach
-k = 1;
+/**
+ * Problem: Calculate the kth largest item in the array
+ */
+// solution [1] fixed window size
 const h3 = new MaxHeap();
-for (const v of flatten) {
-	h3.push(-v);
-	if (h3.size() > k) h3.pop();
+for (const item of arr) {
+    h3.push(-item);
+    if (h3.size() > k) h3.pop();
 }
-console.log(`${k}th largest element is`, -h3.pop()!);
-
-// find kth smallest element using k calls to pop (min heap)
-k = 4;
+console.log(`${k}th largest item is ${-h3.pop()!}`);
+// solution [2] k calls to pop
 const h4 = new MaxHeap();
-for (const v of flatten) h4.push(-v);
-for (let i = 1; i < k; i++) h4.pop();
-console.log(`${k}th smallest element is`, -h4.pop()!);
+for (const item of arr) h4.push(item);
+for (let i = 1; i < k; i += 1) h4.pop();
+console.log(`${k}th largest item is ${h4.pop()!}`)
